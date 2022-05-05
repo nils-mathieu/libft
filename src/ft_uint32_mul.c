6db6114d51fa297557a1675fab25cf8bef1b12dc
nmathieu <nmathieu@student.42.fr>
2022-05-05 16:52:06 +0200

    fix ft_uint32_mul

--- src/ft_uint32_mul.c
+++ src/ft_uint32_mul.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 17:26:41 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/04 17:27:49 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/05 16:51:48 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 bool	ft_uint32_mul(uint32_t a, uint32_t b, uint32_t *result)
 {
+	if (a == 0 || b == 0)
+		return (*result = 0, true);
 	*result = a * b;
 	return (*result / a == b);
 }
