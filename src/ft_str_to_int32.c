d6564e85280584d50370a4407411168f2f13b8f4
nmathieu <nmathieu@student.42.fr>
2022-05-07 11:19:22 +0200

    fix the `ft_str_to_int32` function

--- src/ft_str_to_int32.c
+++ src/ft_str_to_int32.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 11:07:12 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/07 11:11:38 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/07 11:18:55 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 const char	*ft_str_to_int32(const char *s, int32_t *result)
 {
 	bool		neg;
+	const char	*ret;
 
 	neg = false;
 	if (*s == '-')
 		neg = (s++, true);
 	else if (*s == '+')
 		s++;
-	if (!ft_str_to_uint32(s, (uint32_t *)result))
-		return (NULL);
-	if (*result < 0)
+	ret = ft_str_to_uint32(s, (uint32_t *)result);
+	if (!ret || *result < 0)
 		return (NULL);
 	if (neg)
 		*result = -*result;
