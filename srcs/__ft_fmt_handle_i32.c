3f6d1c603e490cfc336051bfbde07869b00bce0d
nmathieu <nmathieu@student.42.fr>
2022-05-14 00:14:13 +0200

    fix ft_uint_to_str

--- srcs/__ft_fmt_handle_i32.c
+++ srcs/__ft_fmt_handle_i32.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 23:25:39 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/14 00:08:00 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/14 00:09:20 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,7 @@ bool	__ft_fmt_handle_i32(t_writer w, va_list args)
 		i = -i;
 		neg = true;
 	}
-	start = ft_uint32_to_str(
-			i,
-			(t_str){"0123456789", 10}, buf + 11);
+	start = ft_uint32_to_str(i, (t_str){"0123456789", 10}, buf + 11);
 	if (neg)
 		*(--start) = '-';
 	return (w.write(w.self, start, 11 - (start - buf)));
--- srcs/ft_uint32_to_str.c
+++ srcs/ft_uint32_to_str.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 21:21:28 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/05 12:38:50 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/14 00:18:55 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_uint32_to_str(uint32_t i, t_str base, char *buf_end)
 {
+	if (i == 0)
+		return (*(--buf_end) = base.data[0], buf_end);
 	while (i)
 	{
 		buf_end--;
