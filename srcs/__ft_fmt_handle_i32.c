cd6978d2a5eade4dfde276bdf7feb0490eb17af5
nmathieu <nmathieu@student.42.fr>
2022-05-14 00:08:18 +0200

    fix `__ft_fmt_handle_i32`

--- srcs/__ft_fmt_handle_i32.c
+++ srcs/__ft_fmt_handle_i32.c
@@ -6,19 +6,33 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 23:25:39 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/13 23:54:57 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/14 00:08:00 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "__libft_fmt.h"
+#include "libft.h"
 
 bool	__ft_fmt_handle_i32(t_writer w, va_list args)
 {
 	char	buf[11];
 	char	*start;
+	bool	neg;
+	int32_t	i;
 
-	start = ft_int32_to_str(
-			va_arg(args, uint32_t),
+	i = va_arg(args, uint32_t);
+	if (i == INT_MIN)
+		return (w.write(w.self, "-2147483648", 11));
+	neg = false;
+	if (i < 0)
+	{
+		i = -i;
+		neg = true;
+	}
+	start = ft_uint32_to_str(
+			i,
 			(t_str){"0123456789", 10}, buf + 11);
+	if (neg)
+		*(--start) = '-';
 	return (w.write(w.self, start, 11 - (start - buf)));
 }
