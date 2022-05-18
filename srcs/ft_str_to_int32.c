8bc59a14f0f79de243c4a69fbe6afeafe9f7c25d
nmathieu <nmathieu@student.42.fr>
2022-05-18 21:20:42 +0200

    fix the `str_to_int32` function failing on INT32_MAX

--- srcs/ft_str_to_int32.c
+++ srcs/ft_str_to_int32.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 11:07:12 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/05/07 11:22:42 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/05/18 21:20:09 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ const char	*ft_str_to_int32(const char *s, int32_t *result)
 	else if (*s == '+')
 		s++;
 	ret = ft_str_to_uint32(s, (uint32_t *)result);
-	if (!ret || *result < 0)
+	if (!ret || (uint32_t)*result > (uint32_t)INT32_MIN)
 		return (NULL);
 	if (neg)
 		*result = -*result;
