0eea431e437cb13403f04c2686d52f71c009c71b
nmathieu <nmathieu@student.42.fr>
2022-07-11 10:36:07 +0200

    fix major bug in `ft_vec_realloc`

--- srcs/ft_vec_realloc.c
+++ srcs/ft_vec_realloc.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 14:09:51 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/06/19 15:09:31 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/07/11 10:35:27 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	ft_vec_realloc(t_vec *vec, size_t new_cap, size_t elem_size)
 {
 	void	*new_data;
 
-	if (new_cap > ((SIZE_MAX / elem_size) - 4) / 2)
+	if (elem_size != 0 && new_cap > SIZE_MAX / elem_size)
 		ft_unwind_panic("the system is out of memory");
-	new_data = ft_alloc(new_cap);
+	new_data = ft_alloc(new_cap * elem_size);
 	if (vec->cap)
 	{
 		ft_mem_copy(new_data, vec->data, elem_size * vec->len);
