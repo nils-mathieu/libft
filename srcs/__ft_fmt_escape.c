79967ef6fc3560d60291fb2b4fc5a3666895a223
nmathieu <nmathieu@student.42.fr>
2022-07-14 12:50:44 +0200

    properly escape the \ character

--- srcs/__ft_fmt_escape.c
+++ srcs/__ft_fmt_escape.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 15:45:51 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/06/19 16:12:39 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/07/14 12:50:31 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,8 @@ t_str	__ft_fmt_escape_ascii(char c)
 		else
 			return (g_escapes[(unsigned char)c]);
 	}
+	else if (c == '\\')
+		return ((t_str){"\\\\", 2});
 	else
-	{
 		return ((t_str){NULL, 0});
-	}
 }
