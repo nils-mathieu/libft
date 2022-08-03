618cb5da2d874ae2907cefd9fe660e60c287ab3d
nmathieu <nils.mathieu.contact@gmail.com>
2022-08-03 20:31:32 +0200

    fix `ft_vec_append`

--- srcs/ft_vec_append.c
+++ srcs/ft_vec_append.c
@@ -6,7 +6,7 @@
 /*   By: nmathieu <nmathieu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 19:32:16 by nmathieu          #+#    #+#             */
-/*   Updated: 2022/07/24 19:33:59 by nmathieu         ###   ########.fr       */
+/*   Updated: 2022/08/03 20:31:21 by nmathieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	ft_vec_append(t_vec *vec, const void *data, size_t n, size_t es)
 {
 	ft_vec_reserve(vec, n, es);
-	ft_mem_copy(vec->data, data, n * es);
+	ft_mem_copy(vec->data + vec->len, data, n * es);
 	vec->len += n;
 }
